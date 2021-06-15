6e8ab7cd0eec1de5890c2eec7327c1f17e32a6dc
Fabio Cataleta <fcataleta@gmail.com>
2021-06-15 16:15:05 +0100

    -Removed redundant files -Removed original styling -Added Firebase hosting params

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC4POiRQvsI2WItn4AhdAh90SZSN0cwXv8",
+  authDomain: "whatsapp-react-60a8e.firebaseapp.com",
+  projectId: "whatsapp-react-60a8e",
+  storageBucket: "whatsapp-react-60a8e.appspot.com",
+  messagingSenderId: "1014307197032",
+  appId: "1:1014307197032:web:d9375ea7c8d549788f6bdd",
+  measurementId: "G-5740HCZ2CZ"
+};
