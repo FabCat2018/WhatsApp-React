3cea83721438cc8b58bf3e7bd014b87bb3df41bc
Fabio Cataleta <fcataleta@gmail.com>
2021-06-15 17:14:38 +0100

    -Added Sidebar search component and styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,7 @@ import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
 
 import "./Sidebar.css";
 
@@ -24,7 +25,12 @@ function Sidebar() {
                 </div>
             </div>
 
-            <div className="sidebar__search"></div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
 
             <div className="sidebar__chats"></div>
         </div>
