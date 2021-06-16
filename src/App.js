9bef86943af1dd0ee32017ac6670dc293ef78af9
Fabio Cataleta <fcataleta@gmail.com>
2021-06-16 11:59:23 +0100

    -Added extra SidebarChat component to Sidebar -Optimised imports for Sidebar -Added Chat header component with styling -Optimised formatting for App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import "./App.css";
+import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+
+import "./App.css";
 
 function App() {
     return (
@@ -8,7 +10,7 @@ function App() {
         <div className="app">
             <div className="app__body">
                 <Sidebar />
-                {/* Chat */}
+                <Chat />
             </div>
         </div>
     );
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Avatar, IconButton } from "@material-ui/core";
-import DonutLargeIcon from "@material-ui/icons/DonutLarge";
-import ChatIcon from "@material-ui/icons/Chat";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from "@material-ui/icons";
 
 import SidebarChat from "./SidebarChat";
 
@@ -16,13 +13,13 @@ function Sidebar() {
                 <Avatar />
                 <div className="sidebar__headerRight">
                     <IconButton>
-                        <DonutLargeIcon />
+                        <DonutLarge />
                     </IconButton>
                     <IconButton>
-                        <ChatIcon />
+                        <Chat />
                     </IconButton>
                     <IconButton>
-                        <MoreVertIcon />
+                        <MoreVert />
                     </IconButton>
                 </div>
             </div>
@@ -36,6 +33,7 @@ function Sidebar() {
 
             <div className="sidebar__chats">
                 <SidebarChat addNewChat />
+                <SidebarChat />
                 <SidebarChat />
                 <SidebarChat />
             </div>
