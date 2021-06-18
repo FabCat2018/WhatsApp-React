7fce3c15b5922647417270a1179b7ee417428e61
Fabio Cataleta <fcataleta@gmail.com>
2021-06-18 12:57:36 +0100

    -Added conditional rendering of Login page if user is not signed in -Implemented Login component with styling -Added Sign in with Google option using Firebase

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import Login from "./Login";
 
 import "./App.css";
 
 function App() {
+    const [user, setUser] = useState(null);
+
     return (
         // BEM naming convention
         <div className="app">
-            <div className="app__body">
-                <Router>
-                    <Sidebar />
-                    <Switch>
-                        <Route path="/rooms/:roomId">
-                            <Chat />
-                        </Route>
-                        <Route path="/">
-                            <Chat />
-                        </Route>
-                    </Switch>
-                </Router>
-            </div>
+            {!user ? (
+                <Login />
+            ) : (
+                <div className="app__body">
+                    <Router>
+                        <Sidebar />
+
+                        <Switch>
+                            <Route path="/rooms/:roomId">
+                                <Chat />
+                            </Route>
+                            <Route path="/">
+                                <Chat />
+                            </Route>
+                        </Switch>
+                    </Router>
+                </div>
+            )}
         </div>
     );
 }
